0a50fe404a0b5fd5702c689655e414c35654e05c
trojanMail <ray13alexander@gmail.com>
2022-04-04 08:44:33 -0500

    Added base unittest class for caesar

---
+++ unittest/TestCaesarEncipher.py
@@ -0,0 +1,13 @@
+import unittest
+
+class TestCaesarEncipher(unittest.TestCase):
+
+    def test_safefail1(self):
+        pass
+    def test_safefail2(self):
+        pass
+    def test_safefail3(self):
+        pass
+    
+if __name__ == '__main__':
+    unittest.main()
